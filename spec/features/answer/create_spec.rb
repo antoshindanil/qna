24d913b69167333da9294dec592f9a9c57fa560c
Danil Antoshin <antoshindanil@ya.ru>
2020-06-11 23:15:51 +0200

    User may add files when creating answer

--- spec/features/answer/create_spec.rb
+++ spec/features/answer/create_spec.rb
@@ -26,6 +26,16 @@ feature "User can create an answer to the question" do
 
       expect(page).to have_content "Body can't be blank"
     end
+
+    scenario "answer with attached files", js: true do
+      fill_in "Body", with: "text text text"
+
+      attach_file "Files", ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+      click_on "Answer"
+
+      expect(page).to have_link "rails_helper.rb"
+      expect(page).to have_link "spec_helper.rb"
+    end
   end
 
   describe "Unauthenticated user" do
