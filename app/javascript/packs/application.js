afb91cd32d79b906e49a765b28d7f0a5aaaa5d7d
Danil Antoshin <antoshindanil@ya.ru>
2020-06-15 22:33:25 +0200

    Render gist if link contain gist.github.com

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,9 @@ require("jquery")
 require("utilities/edit_answer")
 require("utilities/edit_question")
 require("cocoon-js")
+require("gist-embed/dist/gist-embed.min")
+require("utilities/embeded_gist")
+require("bulma/css/bulma.css")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
